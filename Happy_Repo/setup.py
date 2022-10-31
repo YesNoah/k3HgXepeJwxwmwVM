8c4c66a5107df1639e5f6aa1078bd9febc52667c
YesNoah <98677277+YesNoah@users.noreply.github.com>
2022-10-31 12:07:22 -0400

    Add files via upload

---
+++ Happy_Repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying happy and unhappy customers with survey data',
+    author='Noah',
+    license='MIT',
+)
